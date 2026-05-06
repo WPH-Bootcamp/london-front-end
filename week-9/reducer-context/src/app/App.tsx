939a9700f5679ba9c79d37faf8e58f434fa31ad0
vincent-guizot <vincentguizot128@gmail.com>
2026-05-06 20:31:34 +0700

    useReducer

--- week-9/reducer-context/src/app/App.tsx
+++ week-9/reducer-context/src/app/App.tsx
@@ -1,15 +1,15 @@
 // import Combine from "../components/Combine";
 // import ContextAuth from "../components/ContextAuth";
 // import ContextTheme from "../components/ContextTheme";
-import ReducerBasic from "../components/ReducerBasic";
-// import ReducerForm from "../components/ReducerForm";
+// import ReducerBasic from "../components/ReducerBasic";
+import ReducerForm from "../components/ReducerForm";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <ReducerBasic />
-      {/* <ReducerForm /> */}
+      {/* <ReducerBasic /> */}
+      <ReducerForm />
       {/* <ContextTheme /> */}
       {/* <ContextAuth /> */}
       {/* <Combine /> */}
--- week-9/reducer-context/src/components/ReducerForm.tsx
+++ week-9/reducer-context/src/components/ReducerForm.tsx
@@ -1,7 +1,79 @@
-import React from "react";
+import { useReducer } from "react";
 
-const ReducerForm = () => {
-  return <div>ReducerForm</div>;
+type FormState = {
+  fullName: string;
+  email: string;
 };
 
-export default ReducerForm;
+type Action =
+  | {
+      type: "SET_FIELD";
+      field: string;
+      value: string;
+    }
+  | {
+      type: "RESET";
+    };
+
+const initialState: FormState = {
+  fullName: "",
+  email: "",
+};
+
+const formReducer = (state: FormState, action: Action): FormState => {
+  switch (action.type) {
+    case "SET_FIELD":
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+
+    case "RESET":
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+export default function ReducerForm() {
+  const [state, dispatch] = useReducer(formReducer, initialState);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: "SET_FIELD",
+      field: e.target.name,
+      value: e.target.value,
+    });
+  };
+
+  return (
+    <div style={{ border: "1px solid black", padding: "20px" }}>
+      <h1>useReducer Form</h1>
+      <input
+        className="border p-2 borderd-gray-100 rounded-lg"
+        type="text"
+        name="fullName"
+        placeholder="Full Name"
+        value={state.fullName}
+        onChange={handleChange}
+      />
+      <input
+        className="border p-2 borderd-gray-100 rounded-lg"
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={state.email}
+        onChange={handleChange}
+      />
+      <button
+        className="p-2 bg-red-700 text-white hover:bg-red-800 rounded-lg"
+        onClick={() => dispatch({ type: "RESET" })}
+      >
+        Reset
+      </button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
+}
