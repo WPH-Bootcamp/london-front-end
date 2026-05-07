3604e667c73e825aee7a9a45f0545b078b0dc468
vincent-guizot <vincentguizot128@gmail.com>
2026-05-07 20:25:21 +0700

    install shadcn/ui

---
+++ week-9/radix-ui_shadcnui/src/components/ModalShadcn.tsx
@@ -0,0 +1,9 @@
+// npm i @types/node path
+// pastikan install ini terlebih dulu
+import React from "react";
+
+const ModalShadcn = () => {
+  return <div>ModalShadcn</div>;
+};
+
+export default ModalShadcn;
